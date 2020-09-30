eb11ca489dbfe837f0331a50b3655935ad9d2cac
lkvreddy007 <lkvreddy007@gmail.com>
2020-09-30 21:57:07 +0530

    [Krishna]added-exceptionHandilingCapabilityForMoodAnalysis

--- src/main/java/org/capg/MoodAnalyzer.java
+++ src/main/java/org/capg/MoodAnalyzer.java
@@ -1,11 +1,17 @@
 package org.capg;
 
 public class MoodAnalyzer {
-	public String analyseMood(String message) {
-		if(message.contains("sad")) {
-			return "SAD";
-		}else {
-			return "HAPPY";
+	public String analyseMood(String message) throws MoodAnalysisException {
+		try {
+			if(message.contains("sad")) {
+				return "SAD";
+			}else {
+				return "HAPPY";
+			}
+		}
+		catch (Exception e) {
+			System.out.println("Exception");
+			throw new MoodAnalysisException("Please enter proper message");
 		}
 	}
 }

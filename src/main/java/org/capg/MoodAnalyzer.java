0d5166f7c5b5a67b4e815e5ea4ae48bf8a0f0d55
lkvreddy007 <lkvreddy007@gmail.com>
2020-09-30 21:39:37 +0530

    [Krishna]added-moodAnalyzer

---
+++ src/main/java/org/capg/MoodAnalyzer.java
@@ -0,0 +1,11 @@
+package org.capg;
+
+public class MoodAnalyzer {
+	public String analyseMood(String message) {
+		if(message.contains("sad")) {
+			return "SAD";
+		}else {
+			return "HAPPY";
+		}
+	}
+}
